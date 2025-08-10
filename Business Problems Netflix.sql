4acf0ffb618557d68538b0934e7d64ad7e962f23
Praveenl2102 <9921004813@klu.ac.in>
2025-08-10 22:24:48 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -5,7 +5,7 @@
 3. List all movies released in a specific year (e.g., 2020)
 4. Find the top 5 countries with the most content on Netflix
 5. Identify the longest movie
-6. Find content added in the last 5 years
+6. Find content added in the last 10 years
 7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
 8. List all TV shows with more than 5 seasons
 9. Count the number of content items in each genre
@@ -13,7 +13,7 @@
 return top 5 year with highest avg content release!
 11. List all movies that are documentaries
 12. Find all content without a director
-13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+13. Find how many movies actor 'Salman Khan' appeared in last 15 years!
 14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
 15.
 Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
